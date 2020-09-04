2afe1114ff1f47329b5ccf8befcd4adbada4acad
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-09-04 12:17:24 -0500

    fetch from BlogContainer and display blogs from BlogTile, implement CSS for readability

--- src/BlogContainer.js
+++ src/BlogContainer.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogTile from './BlogTile';
 
 const BlogContainer = () => {
+    // const mediumUrl = "https://medium.com/feed/@joshuagauthreaux"
+    const rss2json = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40joshuagauthreaux";
+    const [myBlog, setMyBlog] = useState([]);
+
+    useEffect(() => {
+        fetch(rss2json)
+            .then(res => res.json())
+            .then(data => {
+                setMyBlog(data)
+                // console.log(data)
+            })
+    }, [rss2json]);
+
+    function displayBlogs() {
+        console.log(myBlog)
+        return myBlog.items && myBlog.items.map(blog => {
+            return blog.categories.length > 0 && <BlogTile key={blog.pubDate} blogData={blog} />
+        })
+    }
+
     return (
-        <div>
-            this component will hold all my blogs
-            <BlogTile/>
-            <BlogTile />
-            <BlogTile />
-        </div>
+            <div className="BlogsContainer">
+                {displayBlogs()}
+            </div>
     );
 }
 
--- src/BlogTile.js
+++ src/BlogTile.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
-const BlogTile = () => {
+const BlogTile = (props) => {
+    const { title, link, thumbnail, content, pubDate } = props.blogData;
+
+    function blogPost() {
+        return <a target="_blank" rel="noopener" href={`${link}`}>
+            <div className="ImageContainer">
+                <img src={`${thumbnail}`} className="Image" alt={truncateText(cleanTitle(title), 0, 60)}></img>
+            </div>
+            <div className="TDContainer">
+                <h3>{truncateText(cleanTitle(title), 0, 60)}</h3><br />
+                <p>{truncateText(toText(content), 48, 300) + "..."}</p><br />
+                <h4>{convertDate(pubDate)}</h4>
+            </div>
+        </a>
+
+    }
+
+    function cleanTitle(checkTitle) {
+        checkTitle = checkTitle.replace("amp;", "");
+        return checkTitle
+    }
+
+    function truncateText(text, start, len) {
+        return text.length > len ? text.slice(start, len) : text;
+    }
+
+    function toText(block) {
+        let tag = document.createElement('div');
+        tag.innerHTML = block;
+        block = tag.innerText;
+        return block
+    }
+
+    function convertDate(date) {
+        let dateArray = date.slice(0, 10).split("-")
+        let year = dateArray.shift();
+        dateArray.push(year)
+        console.log(date)
+        return `Published: ${dateArray.join("/")}`;
+    }
+
     return (
-        <div>
-            this is a single blog
+        <div className="PostContainer">
+            {blogPost()}
         </div>
     );
 }
